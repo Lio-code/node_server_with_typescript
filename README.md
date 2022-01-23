3841d8ab33adb2c6b4f3dc08b8dd3d6a2dc16440
Lionel <lio4455@hotmail.com>
2022-01-23 21:22:08 +0100

    creation of first route and controller

---
+++ README.md
@@ -0,0 +1,6 @@
+#Dependencies
+express
+
+#dev dependencies
+@types/express
+@types/node
---
+++ src/controllers/todos.ts
@@ -0,0 +1,13 @@
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import { Todo } from '../models/todos';
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+
+  res.status(201).json({ message: 'new todo created', createdTodo: newTodo });
+};
