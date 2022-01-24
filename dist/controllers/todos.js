4c25ed6855a3e744e4526a4554da4c25c3acae1b
Lionel <lio4455@hotmail.com>
2022-01-24 13:15:56 +0100

    route for update todo set

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTodos = exports.createTodo = void 0;
+exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 //Interface RequestHandler from express expects 3 parameters: Request, Response, NextFunction
 const todos_1 = require("../models/todos");
-const TODOS = [];
+let TODOS = [];
 const createTodo = (req, res, next) => {
     const text = req.body.text;
     const newTodo = new todos_1.Todo(Math.random().toString(), text);
@@ -17,3 +17,14 @@ const getTodos = (req, res, next) => {
         .json({ message: 'access to todo list granted', todos: TODOS });
 };
 exports.getTodos = getTodos;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex((el) => el.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('todo does not exist');
+    }
+    TODOS[todoIndex] = new todos_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.json({ message: 'todo updated', updatedTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from 'express';
 
 import { Todo } from '../models/todos';
 
-const TODOS: Todo[] = [];
+let TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
@@ -18,4 +18,20 @@ export const getTodos: RequestHandler = (req, res, next) => {
   res
     .status(200)
     .json({ message: 'access to todo list granted', todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((el) => el.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error('todo does not exist');
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: 'todo updated', updatedTodo: TODOS[todoIndex] });
 };
